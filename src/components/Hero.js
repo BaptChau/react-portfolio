92a105f70ca5691c9d233cf76ecbc1cf77de2883
Baptiste Chaudron <chaudron964@gmail.com>
2020-11-02 15:59:42 +0100

    ajout d'un avatar à la page d'accueil

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import { Col,Row, Container, Jumbotron } from 'react-bootstrap'
 
-function Hero(props){
+function Hero(props, Children){
     let prop = props.info
     return [
-    <Jumbotron className="bg-transparent jumbotron-fluid">
+    <Jumbotron className="bg-transparent jumbotron-fluid text-center">
         <Container>
             <Row>
                 <Col>
                     <h1>{prop.title}</h1>
-                    {prop.subtitle ? <h2>{prop.subtitle}</h2>: ''}
-                    {prop.text ? <h3>{prop.text}</h3>: ''}
-
+                    {prop.subtitle ? <cite>{prop.subtitle}</cite>: ''}
+                    {prop.text ? <h5>{prop.text}</h5>: ''}
                 </Col>
             </Row>
         </Container>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import photo from '../assets/avatar-gratuit.png'
+import { Row, Col, Container } from 'react-bootstrap';
+function HomePage(props) {
+    let prop = props.info
+    return [
+        <Container className="mt-5">
+            <Row>
+                <Col>
+                    <Hero info={prop} />
+                </Col>
+                <Col>
+                    <img src={photo} alt="moi" width="250" height="250" />
+                </Col>
+            </Row>
 
-function HomePage(props){
-let prop = props.info
-return <Hero info= {prop}/>
+        </Container>
+
+    ]
 }
 
 export default HomePage;
